9a0af9387d0894a4a063ff86d428b71bb1714e37
Kim-Anh Tran <kim-anh.tran@hotmail.de>
2013-05-13 17:34:04 +0200

    Changed script to take integers instead of lists.

--- scripts/generate_points.py
+++ scripts/generate_points.py
@@ -10,20 +10,20 @@ filename='data'
 def main(argv):
 
     parser = argparse.ArgumentParser(description='Generates test instances.')
-    parser.add_argument('size', metavar='S', type=int, nargs=1, default=30,
+    parser.add_argument('size', metavar='S', type=int, default=30,
                         help='Size of the problem instance.')
-    parser.add_argument('-l', '--lower', metavar='L', type=int, nargs=1,
+    parser.add_argument('-l', '--lower', metavar='L', type=int, 
                         default=10, help='Lower bound of coordinate.')
-    parser.add_argument('-u', '--upper', metavar='U', type=int, nargs=1,
+    parser.add_argument('-u', '--upper', metavar='U', type=int, 
                         default=0, help='Upper bound of coordinate.')
-    parser.add_argument('-n', '--number', metavar='N', type=int, nargs=1,
+    parser.add_argument('-n', '--number', metavar='N', type=int, 
                         default=1,help='Number of instances to generate.')
-    parser.add_argument('-p', '--prefix', metavar='P', type=str, nargs=1,
+    parser.add_argument('-p', '--prefix', metavar='P', type=str, 
                         help='Prefix of file to store instance in.')
     args = parser.parse_args()
     
     for i in range(0,args.number):
-        generated = generate(args.size[0], args.lower, args.upper, 4)
+        generated = generate(args.size, args.lower, args.upper, 4)
         writeToFile(directory + filename + str(i), generated)
 
 def writeToFile(filename, data):
@@ -37,8 +37,8 @@ def generate(size, lower, upper, colors):
     data = []
 
     for i in range(0, size):
-        x = (round(random.uniform(lower[0], upper[0]), 6))
-        y = (round(random.uniform(lower[0], upper[0]), 6))
+        x = (round(random.uniform(lower, upper), 6))
+        y = (round(random.uniform(lower, upper), 6))
         c = (random.randint(0,colors))
         data.append((x,y,c))
 
