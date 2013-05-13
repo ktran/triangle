6e4738b97a7ced09882e316bc73de622324259e3
Kim-Anh Tran <kim-anh.tran@hotmail.de>
2013-05-13 15:54:14 +0200

    halway tostring

--- geometrics/src/geometry/point/ColoredPointImpl.java
+++ geometrics/src/geometry/point/ColoredPointImpl.java
@@ -3,12 +3,30 @@ package geometry.point;
 import color.Color;
 import com.vividsolutions.jts.geom.Coordinate;
 
+import java.util.StringTokenizer;
+
 /**
  * A ColoredPoint implementation.
  *
  * @author Kim-Anh Tran
  */
 public class ColoredPointImpl implements ColoredPoint {
+
+    /**
+     * Minimum number of tokens needed for creating a ColoredPoint instance from
+     * a String representation.
+     *
+     * @see ColoredPointImpl#valueOf(String)
+     */
+    private static int MIN_TOKENS = 3;
+
+    /**
+     * Maximum number of tokens for creating a ColoredPoint instance
+     * from a String representation.
+     *
+     * @see ColoredPointImpl#valueOf(String)
+     */
+    private static int MAX_TOKENS = 4;
 
     /**
      * The coordinate of the point.
@@ -32,6 +50,21 @@ public class ColoredPointImpl implements ColoredPoint {
         this.color = color;
     }
 
+    public static ColoredPointImpl valueOf(String stringRepresentation) {
+        double x, y, z;
+        int color;
+
+        StringTokenizer tokenizer = new StringTokenizer(stringRepresentation);
+        int nTokens = tokenizer.countTokens();
+
+        if (nTokens < MIN_TOKENS || nTokens > MAX_TOKENS)
+            throw new IllegalArgumentException("Unexpected number of tokens.");
+
+            tokenizer.nextElement();
+
+        return null;
+    }
+
     @Override
     public Coordinate getCoordinate() {
         return this.coordinate;
@@ -48,5 +81,6 @@ public class ColoredPointImpl implements ColoredPoint {
         Coordinate pointCoord = coloredPoint.getCoordinate();
         return this.coordinate.compareTo(pointCoord);
     }
+
 }
 
